86bcb278fb60aee5a43cf0d3b0059284920e7e2c
noctis0430 <noctis0430@yahoo.com>
2020-06-11 12:19:33 +0800

    Bumped version.

--- src/PdfSharp/root/ProductVersionInfo.cs
+++ src/PdfSharp/root/ProductVersionInfo.cs
@@ -120,7 +120,7 @@ namespace PdfSharp
         /// <summary>
         /// The patch number of the product.
         /// </summary>
-        public const string VersionPatch = "0";
+        public const string VersionPatch = "1";
 
         /// <summary>
         /// The Version Prerelease String for NuGet.
